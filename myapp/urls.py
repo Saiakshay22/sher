e0c50051c4c2089a6ce8fd60d595345a20e83909
Saiakshay22 <akshaysai029@gmail.com>
2024-12-23 12:11:42 +0530

    new

---
+++ myapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from.views import *
+app_name='myapp'
+
+urlpatterns=[
+
+
+    path('',home,name='home'),
+
+    path('courses/',courses,name='courses'),
+
+    path('bootcamp/',bootcamp,name='bootcamp'),
+
+    path('requestcallback/',requestcallback,name='requestcallback'),
+
+    path('signin/',signin,name='signin')
+
+]
